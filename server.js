e5cd1bef69eade277f9eb4c6e4b4169d615e6da6
Andy Le <andylequ@gmail.com>
2026-03-25 15:16:50 -0500

    added pings for checking if the connection should be kept alive.

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ wss.on('connection', (socket) => {
     clients.add(socket);
     console.log('Client connected. Total:',clients.size);
 
+    socket.isAlive = true;
+    socket.on('pong', () => { socket.isAlive = true;});
+
+    setInterval(() => {
+        for(const socket of wss.clients) {
+            if(!socket.isAlive) {socket.terminate(); return;}
+            socket.isAlive = false;
+            socket.ping();
+        }
+    }, 30000);
+
     socket.on('message', (raw) => {
         const message = JSON.parse(raw);
         for(const client of clients) {
